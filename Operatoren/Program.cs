d2d706968c7472c5380301ea7d22816faafc7da9
zwebsites.ch <dzurmuehle@gmail.com>
2023-11-09 20:03:54 +0100

    Add project files.

---
+++ Operatoren/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 43;
+            int y = 5;
+            Console.WriteLine(x / y);
+        }
+    }
+}
---
+++ TaschenrechnerUebung/taschenrechner.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Taschenrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Einfacher Taschenrechner");
+
+            while (true)
+            {
+                Console.Write("Geben Sie den ersten Operanden ein: ");
+                if (!double.TryParse(Console.ReadLine(), out double operand1))
+                {
+                    Console.WriteLine("Ungültige Eingabe für den ersten Operanden.");
+                    continue;
+                }
+
+                Console.Write("Geben Sie den zweiten Operanden ein: ");
+                if (!double.TryParse(Console.ReadLine(), out double operand2))
+                {
+                    Console.WriteLine("Ungültige Eingabe für den zweiten Operanden.");
+                    continue;
+                }
+
+                Console.WriteLine("Wählen Sie eine Operation:");
+                Console.WriteLine("1 - Addition");
+                Console.WriteLine("2 - Subtraktion");
+                Console.WriteLine("3 - Multiplikation");
+                Console.WriteLine("4 - Division");
+
+                int operationChoice;
+                if (!int.TryParse(Console.ReadLine(), out operationChoice) || operationChoice < 1 || operationChoice > 4)
+                {
+                    Console.WriteLine("Ungültige Operation ausgewählt.");
+                    continue;
+                }
+
+                double result = 0.0;
+
+                switch (operationChoice)
+                {
+                    case 1:
+                        result = operand1 + operand2;
+                        break;
+                    case 2:
+                        result = operand1 - operand2;
+                        break;
+                    case 3:
+                        result = operand1 * operand2;
+                        break;
+                    case 4:
+                        if (operand2 != 0)
+                        {
+                            result = operand1 / operand2;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nDivision durch Null ist nicht erlaubt.");
+                            continue;
+                        }
+                        break;
+                }
+
+                Console.WriteLine($"\nErgebnis: {result:F2}");
+
+                Console.Write("Möchten Sie eine weitere Berechnung durchführen? (J/N): ");
+                char response = Console.ReadKey().KeyChar;
+                if (response != 'J' && response != 'j')
+                {
+                    break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
